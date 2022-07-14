d4d960da2f8861855d3fb0cfc470bff11e2a88cb
Your Rimanik <therimanik@gmail.com>
2022-07-15 00:08:39 +0500

    chore(project): Cofigure simple chat example.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.info("Hello")
+import { Telegraf } from "telegraf"
+
+const bot = new Telegraf("5456109251:AAFub0hhQzLuf-OU2yLOOfZBXlUYSeunVyM")
+bot.start((ctx) => ctx.reply('HEllo bro2!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('PTIVET!'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
